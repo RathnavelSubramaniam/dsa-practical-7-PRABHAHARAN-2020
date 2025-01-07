6b2ca5d2eed7a191851071e49dc0a4bae9f637e6
PRABHAHARAN A <122508061+PRABHAHARAN-2020@users.noreply.github.com>
2025-01-07 06:32:13 +0000

    CMT2

--- LinkedList.java
+++ LinkedList.java
@@ -78,21 +78,13 @@ public void insertMiddle(int data,int pos)
         Node newNode = new Node(data);    
        //temporary node points to head node
         Node temp=head;
-        if(pos==1)
-        {
-newNode.next=temp;
-        head=newNode;
-        }
-        else
-        {
         for(int i=1;i<(pos-1);i++ ) 
         {    
             temp = temp.next;    
-        }    
+        }   
 newNode.next=temp.next;
 temp.next=newNode;
-        }
-System.out.println("Element Inserted");
+System.out.println("ELEMENT INSERTED");
     }
 
 public void displayList() {
